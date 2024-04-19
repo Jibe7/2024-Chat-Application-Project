e0b0b19d848a8231b2fb61c6ca697811cd2bfc62
Jibe7 <jb7_chevrier@hotmail.com>
2024-04-20 00:54:28 +0200

    mainly re-organized folders and files

---
+++ MiniProjects/Database Test/readme.md
@@ -0,0 +1,5 @@
+## Motivation
+
+Learn to interact directly with a database and use "low-level" tools (compared to frameworks where everything is build)
+
+Learn to apply OOP and design myself a system to apply my designs skills (try to apply SOLID, design patterns)
---
+++ MiniProjects/express-test/src/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+      secure: false, // Set to true for https environments
+      maxAge: 60000 * 60,
+    } 
+};
+app.use(cookieParser('secret'));
+app.use(session(sessionConfig));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json())
+
+app.post('/login', (request, response) => {
+    console.log(request.cookies)
+    console.log(request.signedCookies)
+    // console.log(request.session)
+    console.log(request.sessionID)
+    console.log(request.body)
+    // console.log(request.body.password)
+    // console.log(request.body.username)
+    console.log("Hello")
+    response.sendStatus(200)
+    // response.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.post('/register', (request, response) => {
+    console.log(request.cookies)
+    console.log(request.signedCookies)
+    // console.log(request.session)
+    console.log(request.sessionID)
+    console.log("Hello")
+    // response.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+module.exports = app;
